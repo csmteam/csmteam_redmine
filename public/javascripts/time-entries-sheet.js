89205281180ca0e0aa29cdfd77205dd93ed84aa5
Rustam Galiev <qweromail@gmail.com>
2018-02-19 00:14:44 +0300

    Upd range

--- public/javascripts/time-entries-sheet.js
+++ public/javascripts/time-entries-sheet.js
@@ -8,8 +8,8 @@ $(document).ready(function(){
 
   $("#week-picker").scrollableCalendar({
       touch: false,
-      startDate: "2018-02-05",
-      endDate: "2018-04-01",
+      startDate: "2018-01-01",
+      endDate: "2018-12-30",
       currentWeek: "2018-02-19",
       onClick: weekPicked
   });
