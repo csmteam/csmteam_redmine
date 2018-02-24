e7fa2b3cac2d208035334425734c734a8a7d12c9
Rustam Galiev <qweromail@gmail.com>
2018-02-24 18:12:05 +0300

    Enabled plugins

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module RedmineApp
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
-    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+    config.plugins = [ :all ]
 
     config.active_record.store_full_sti_class = true
     config.active_record.default_timezone = :local
