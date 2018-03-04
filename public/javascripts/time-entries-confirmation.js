fc1e95c00086b401765b3ef7ee979cf7e78d15df
Rustam Galiev <qweromail@gmail.com>
2018-03-05 00:46:08 +0300

    Updated TE Confirmation select

--- public/javascripts/time-entries-confirmation.js
+++ public/javascripts/time-entries-confirmation.js
@@ -1,5 +1,5 @@
 function weekPicked(){
-  $("#time-entries-form").attr("action","/time_entries_confirmation/entries/" + this)
+  $("#time-entries-form").attr("action","/time_entries_confirmation/entries/" + $('#week-select').val())
   $("#time-entries-form").submit()
 }
 
@@ -10,12 +10,7 @@ window.formReloaded = function(){
 $(document).ready(function(){
 
   formReloaded()
-  $("#week-picker").scrollableCalendar({
-      touch: false,
-      startDate: "2018-01-01",
-      endDate: "2018-12-30",
-      currentWeek: $("#week-picker").data("current-date"),
-      onClick: weekPicked
-  });
+  $('#week-select').change(weekPicked)
+
 
 })
